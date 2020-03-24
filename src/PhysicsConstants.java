283cc736af20f59e65f683539db4e74f5ebd24c7
jestes15 <sagepup0620@gmail.com>
2020-03-24 13:56:32 -0500

    Fixed case 12 definition

--- src/PhysicsConstants.java
+++ src/PhysicsConstants.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class PhysicsConstants {
     private double G;
     private String H;
 
     public PhysicsConstants(int getInteger) {
+        Scanner sc = new Scanner(System.in);
 
         if (getInteger == 0) {
             this.G = 1.660539040 * Math.pow(10, -27);
@@ -19,6 +22,23 @@ public class PhysicsConstants {
         if (getInteger == 3) {
             this.G = 1.6021766208 * Math.pow(10, -19);
             this.H = "C";
+        }
+        if (getInteger == 4) {
+            this.G = 9.648533289 * Math.pow(10, 4);
+            this.H = "C/mol";
+        }
+        if (getInteger == 5) {
+            System.out.println("a1: Gas Constant in terms of L-atm/mol-K");
+            System.out.println("a2: Gas Constant in terms of J/mol-K");
+            String getChoice = sc.next();
+            if (getChoice == "a1") {
+                this.G = 0.0820582;
+                this.H = "L-atm/mol-K";
+            }
+            if (getChoice == "a2") {
+                this.G = 9.3144598;
+                this.H = "J/mol-K";
+            }
         }
 
     }
